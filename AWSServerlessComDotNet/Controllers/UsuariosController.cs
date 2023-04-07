e224534eb7e031105cda505ac1cfa10c8628b858
Geovani Soares (WebSoares) <72084395+Geovani22446688@users.noreply.github.com>
2023-04-07 14:32:29 -0300

    Adicionar arquivos de projeto.

---
+++ AWSServerlessComDotNet/Controllers/UsuariosController.cs
@@ -0,0 +1,16 @@
+﻿using AWSServerlessComDotNet.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSServerlessComDotNet.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsuariosController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+
+            return Usuarios.retornaUsuarios();
+        }
+    }
+}
---
+++ AWSServerlessComDotNet/Model/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AWSServerlessComDotNet.Model
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DataNascimento { get; set; }
+        public static string retornaUsuarios()
+        {
+            List<Usuarios> usuario = new List<Usuarios>();
+            usuario.Add(new Usuarios() { Id = 01, Name = "Fulano de tal", DataNascimento = "1992-04-20" });
+            usuario.Add(new Usuarios() { Id = 02, Name = "Seclano de tal", DataNascimento = "1989-02-18" });
+
+            string usuarioJson = JsonSerializer.Serialize(usuario);
+
+            return usuarioJson;
+        }
+    }
+}
